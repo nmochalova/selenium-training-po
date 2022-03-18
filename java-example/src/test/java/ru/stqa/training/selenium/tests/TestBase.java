31c526743b034479d6b2c7cd944761ea058578ca
NatalyM <nbelokopytova@gmail.com>
2022-03-18 12:27:54 +0300

    комментарии

--- java-example/src/test/java/ru/stqa/training/selenium/tests/TestBase.java
+++ java-example/src/test/java/ru/stqa/training/selenium/tests/TestBase.java
@@ -5,19 +5,20 @@ import ru.stqa.training.selenium.app.Application;
 
 public class TestBase {
 
-    public static ThreadLocal<Application> tlApp = new ThreadLocal<>();
+    public static ThreadLocal<Application> tlApp = new ThreadLocal<>(); //явные ожидания
     public Application app;
 
     @Before
     public void start() {
+        //если драйвер уже есть, то выходим
         if (tlApp.get() != null) {
             app = tlApp.get();
             return;
         }
-
+        //иначе создаем новый
         app = new Application();
         tlApp.set(app);
-
+        //останов драйвера в конце теста
         Runtime.getRuntime().addShutdownHook(
                 new Thread(() -> { app.quit(); app = null; }));
     }
